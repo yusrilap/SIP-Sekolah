3a26eaf5643253edfe46bdbc3173863f723042d1
yusrilap <37561917+yusrilap@users.noreply.github.com>
2023-09-21 14:00:12 +0700

    First Commit Sistem Informasi Penjadwalan Sekolah

---
+++ database/seeds/PaketSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PaketSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('paket')->insert([
+            'id' => 1,
+            'ket' => 'ATPH',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 2,
+            'ket' => 'APHP',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 3,
+            'ket' => 'ATU',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 4,
+            'ket' => 'TKJ',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 5,
+            'ket' => "APL",
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 6,
+            'ket' => 'GAFIKA',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
